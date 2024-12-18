8afa1da6f4748bc3d07a0edfd47bc3d626732d2c
DongHyeon Lee <idleh4021@naver.com>
2024-12-18 14:28:24 +0900

    초기 커밋.

---
+++ NsisSample/Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NsisSample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = "축하드립니다!\n프로그램이 정상 실행되었습니다.\n\n이제 끄세요.";
+        }
+    }
+}
